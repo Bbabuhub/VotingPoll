c3d7fd3ccf2575b03cbca17ff3ef92e0db92ca23
Balaji Babu <balaji.babu1998@gmail.com>
2019-05-22 15:47:30 -0400

    Incorporated Stylesheets to add some pazaz

--- PollForVoting/polls/tests.py
+++ PollForVoting/polls/tests.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 
 from .models import Question
 
+
 def create_question(question_text, days):
     """
     Create a question with the given `question_text` and published the
@@ -111,7 +112,7 @@ class QuestionDetailViewTests(TestCase):
         The detail view of a question with a pub_date in the future
         returns a 404 not found.
         """
-        future_question = create_question(question_text='Future question.', days=5)
+        future_question = create_question(question_text='Future question.', days=5) # noqa
         url = reverse('polls:detail', args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
@@ -121,7 +122,7 @@ class QuestionDetailViewTests(TestCase):
         The detail view of a question with a pub_date in the past
         displays the question's text.
         """
-        past_question = create_question(question_text='Past Question.', days=-5)
+        past_question = create_question(question_text='Past Question.', days=-5) # noqa
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
